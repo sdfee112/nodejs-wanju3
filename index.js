0ca86b7310f0c57ab29c31797c782cb3ef34120b
sdfee112 <162119699+sdfee112@users.noreply.github.com>
2024-07-06 21:30:16 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,23 +2,24 @@ const express = require("express");
 const app = express();
 const exec = require("child_process").exec;
 const os = require("os");
+
 const port = process.env.SERVER_PORT || process.env.PORT || 3000;
 
 
-console.log(==============================);
+console.log(`==============================`);
 console.log(``);
-console.log("     /stas 查看进程");
-console.log("     /listen 查看端口");
+console.log("     /stas View processes");
+console.log("     /listen Check ports");
 console.log("     /start 手动启动脚本");
 console.log(``);
-console.log(==============================);
+console.log(`==============================`);
 
 app.get("/", function (req, res) {
   res.send("hello world");
 });
 
 app.get("/stas", function (req, res) {
-  let cmdStr = "ps aux | sed 's@--token.*@--token ${TOK}@g;s@-s data.*@-s ${NEZHA_SERVER}@g'";
+  let cmdStr = "ps aux | sed 's@--token.*@--token ${TOK}@g;s@-s.*@-s ${NEZHA_SERVER}@g'";
   exec(cmdStr, function (err, stdout, stderr) {
     if (err) {
       res.type("html").send("<pre>命令行执行错误：\n" + err + "</pre>");
@@ -65,18 +66,17 @@ app.get("/start", (req, res) => {
   });
 
   startScript.stderr.on("data", (data) => {
-    res.write(stderr: ${data});
+    res.write(`stderr: ${data}`);
   });
 
   startScript.on("close", (code) => {
-    res.write(Child process exited with code ${code});
+    res.write(`Child process exited with code ${code}`);
     res.end();
   });
 });
 
 
-
-// 自动启动init.sh脚本
+// 自动启动 start.sh 脚本
 const autoStartScript = exec("sh /app/init.sh");
 
 autoStartScript.stdout.on("data", (data) => {
@@ -84,13 +84,13 @@ autoStartScript.stdout.on("data", (data) => {
 });
 
 autoStartScript.stderr.on("data", (data) => {
-  console.error(stderr: ${data});
+  console.error(`stderr: ${data}`);
 });
 
 autoStartScript.on("close", (code) => {
-  console.log(Child process exited with code ${code});
+  console.log(`Child process exited with code ${code}`);
 });
 
 app.listen(port, () => {
-  console.log(Example app listening on port ${port}!\n==============================);
+  console.log(`Example app listening on port ${port}!\n==============================`);
 });
